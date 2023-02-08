393f6e4c587d8e79968d79b11903a867dcf02045
Laman Taghiyeva <lamantaghiyevaltaghiyeva@Lamans-MacBook-2.local>
2023-02-08 16:38:14 +0400

    Added Week folders & First Spring Application

---
+++ Week3/Your_First_Spring_App/src/main/java/az/edu/ada/wm2/Your_First_Spring_App/YourFirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.Your_First_Spring_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourFirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourFirstSpringAppApplication.class, args);
+	}
+
+}
---
+++ Week3/Your_First_Spring_App/src/test/java/az/edu/ada/wm2/Your_First_Spring_App/YourFirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.Your_First_Spring_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourFirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
